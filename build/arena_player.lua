69f8fee672fd9a3d0c6a6c6ce594b6599e34d009
Damian Galarza <damian@thoughtbot.com>
2015-02-27 15:15:35 -0500

    Build addon

---
+++ build/arena_player.lua
@@ -0,0 +1,21 @@
+do
+  local _base_0 = {
+    spec = nil,
+    name = nil
+  }
+  _base_0.__index = _base_0
+  local _class_0 = setmetatable({
+    __init = function() end,
+    __base = _base_0,
+    __name = "ArenaPlayer"
+  }, {
+    __index = _base_0,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  ArenaPlayer = _class_0
+end
---
+++ build/arena_tracker.lua
@@ -0,0 +1,44 @@
+do
+  local _base_0 = {
+    joined_arena = function(self)
+      if not self.currentMatch then
+        self.currentMatch = ArenaMatch()
+      end
+      self.currentMatch.prepare()
+      return self.currentMatch
+    end,
+    left_arena = function(self)
+      print("left arena")
+      self.currentMatch = nil
+    end,
+    score_updated = function(self)
+      local winner = self.currentMatch:get_winner()
+      if winner and not self.currentMatch.saved then
+        return self:track_match()
+      end
+    end,
+    track_match = function(self)
+      self.currentMatch.saved = true
+      self.currentMatch:determine_result()
+      return table.insert(self.matches, self.currentMatch:toTable())
+    end
+  }
+  _base_0.__index = _base_0
+  local _class_0 = setmetatable({
+    __init = function(self)
+      self.matches = { }
+      return print("Welcome to ArenaTracker")
+    end,
+    __base = _base_0,
+    __name = "ArenaTracker"
+  }, {
+    __index = _base_0,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  ArenaTracker = _class_0
+end
